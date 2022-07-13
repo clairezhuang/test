b4f154b328028b9ef72b96dd4d3993e637a6620b
clairezhuang <109064410+clairezhuang@users.noreply.github.com>
2022-07-13 11:38:55 +0800

    Update test1.py

--- notebook/test1.py
+++ notebook/test1.py
@@ -1,3 +1,4 @@
+# Databricks notebook source
 babynames = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("dbfs:/FileStore/babynames.csv")
 babynames.createOrReplaceTempView("babynames_table")
 years = spark.sql("select distinct(Year) from babynames_table").rdd.map(lambda row : row[0]).collect()
