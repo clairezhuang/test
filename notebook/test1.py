5284b514579a022b4abf195831176f048fb6e0e4
clairezhuang <clairezhuang>
2023-11-03 01:56:52 +0000

    1

--- notebook/test1.py
+++ notebook/test1.py
@@ -2,3 +2,4 @@
 babynames = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("dbfs:/FileStore/babynames.csv")
 babynames.createOrReplaceTempView("babynames_table")
 years = spark.sql("select distinct(Year) from babynames_table").rdd.map(lambda row : row[0]).collect()
+#1
