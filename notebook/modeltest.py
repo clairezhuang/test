31f9cc8033ac7705e3878728fd36111f8896b2dc
clairezhuang <109064410+clairezhuang@users.noreply.github.com>
2024-02-06 22:24:29 +0800

    Update modeltest.py

--- notebook/modeltest.py
+++ notebook/modeltest.py
@@ -6,8 +6,9 @@ import random
 import shutil
 now = datetime.datetime.now()
 task_id = now.strftime("%y%m%d%H%M%S") + str(random.randint(100, 999))
+tmp="/dbfs/FileStore/11/22"
 path= "/Volumes/ml/default/volume1/"
-tmp_path = rf"{task_id}.h5"
+tmp_path = rf"{tmp}/{task_id}.h5"
 model2 = tf.keras.models.load_model("/Volumes/ml/default/volume1/mnist-model.h5")
 save_model(model2, tmp_path)
 print("test1")
