a1cb4734a17fb26c8c2914a53864f98b0da5a9a9
clairezhuang <109064410+clairezhuang@users.noreply.github.com>
2024-02-08 15:00:17 +0800

    Update modeltest.py

--- notebook/modeltest.py
+++ notebook/modeltest.py
@@ -1,7 +1,4 @@
 # Databricks notebook source
-# MAGIC %sh ls
-
-# COMMAND ----------
 
 from tensorflow.keras.models import Model, save_model
 import tensorflow as tf
@@ -11,7 +8,7 @@ import shutil
 now = datetime.datetime.now()
 task_id = now.strftime("%y%m%d%H%M%S") + str(random.randint(100, 999))
 path= "/Volumes/ml/default/volume1/"
-tmp_path = rf"{task_id}.h5"
+tmp_path = rf"/databricks/driver/{task_id}.h5"
 model2 = tf.keras.models.load_model("/Volumes/ml/default/volume1/mnist-model.h5")
 save_model(model2, tmp_path)
 print("test1")
